611409407000bc44ab62c35eef044111694ba130
Will Soto <willsoto@users.noreply.github.com>
2020-11-01 20:07:24 -0500

    chore(*): enable prettier
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,20 @@
+const path = require("path");
+
+const rushPrettierNodeModulesPath = path.join(
+  __dirname,
+  "common",
+  "autoinstallers",
+  "rush-prettier",
+  "node_modules",
+);
+
 module.exports = {
   singleQuote: false,
   trailingComma: "all",
   arrowParens: "always",
   semi: true,
+  plugins: [
+    path.join(rushPrettierNodeModulesPath, "prettier-plugin-packagejson"),
+    path.join(rushPrettierNodeModulesPath, "prettier-plugin-organize-imports"),
+  ],
 };
