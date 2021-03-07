1e765ab1ff957281ef9cc6bb35e34271acb662a3
Will Soto <willsoto@users.noreply.github.com>
2021-03-07 17:52:42 -0500

    chore(*): upgrade all dependencies
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

--- tools/eslint/index.js
+++ tools/eslint/index.js
@@ -7,7 +7,6 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier",
-    "prettier/@typescript-eslint",
   ],
   rules: {
     "tsdoc/syntax": "warn",
