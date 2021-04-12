a4db517f7897b51ec40570c2e87fe85d3f165c76
Will Soto <willsoto@users.noreply.github.com>
2021-04-12 07:39:58 -0400

    chore(examples): fix test
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

--- examples/nestjs/src/app.controller.spec.ts
+++ examples/nestjs/src/app.controller.spec.ts
@@ -1,14 +1,13 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { AppModule } from "./app.module";
 
 describe("AppController", () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
+      imports: [AppModule],
     }).compile();
 
     appController = app.get<AppController>(AppController);
@@ -16,7 +15,15 @@ describe("AppController", () => {
 
   describe("root", () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe("Hello World!");
+      expect(appController.getHello()).toEqual({
+        database: {
+          database: "development",
+          host: "localhost",
+          password: "development",
+          port: 5432,
+        },
+        name: "nestjs-example-development",
+      });
     });
   });
 });
