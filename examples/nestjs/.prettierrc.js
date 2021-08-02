db655ba9c053ef485de8e40617568ddd858abdef
Will Soto <willsoto@users.noreply.github.com>
2021-08-02 17:38:21 -0400

    chore(examples): delete Nest.js example
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

--- examples/nestjs/.prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  trailingComma: "all",
-  arrowParens: "always",
-  plugins: [
-    require.resolve("prettier-plugin-organize-imports"),
-    require.resolve("prettier-plugin-packagejson"),
-  ],
-};
