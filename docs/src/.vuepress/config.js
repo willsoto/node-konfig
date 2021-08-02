a7ff1bf0e62ce78f4325f0166c31833f62d20977
Will Soto <willsoto@users.noreply.github.com>
2021-08-02 17:31:52 -0400

    chore(docs): upgrade Vuepress to beta
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -93,8 +93,5 @@ module.exports = {
     },
   },
 
-  /**
-   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
-   */
-  plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom"],
+  plugins: [],
 };
